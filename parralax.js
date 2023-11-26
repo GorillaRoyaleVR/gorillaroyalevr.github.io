dec87e3771faaee4775338cbae96174ed3fa8509
GorillaRoyaleVR <151435440+GorillaRoyaleVR@users.noreply.github.com>
2023-11-26 06:53:36 +0100

    Add files via upload

--- parralax.js
+++ parralax.js
@@ -1,7 +1,7 @@
-const parralax = document.getElementById("volcano");
+const parralax = document.getElementById("top");
 
 window.addEventListener("scroll", function(){
     console.log(window.pageYOffset);
     let offset = window.pageYOffset;
-    parralax.style.transform = "translateY(" + (-offset) + "px)";
+    parralax.style.transform = "translateY(" + (offset * 0.5) + "px)";
 });
